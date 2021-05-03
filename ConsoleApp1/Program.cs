855437dc5ea1146bffec6f43250c3692e45e41a6
Jian Choi <s2019s19@e-mirim.hs.kr>
2021-05-03 14:58:05 +0900

    #17

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intArray = { 1, 540, 33, 36, 20 };
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[1]);
+            Console.WriteLine(intArray[2]);
+            Console.WriteLine(intArray[3]);
+            Console.WriteLine(intArray[4]);
+            Console.WriteLine(intArray.Length);
+
+            int[] intArray2 = new int[1000];
+            Console.WriteLine(intArray2[0]);
+            Console.WriteLine(intArray2[999]);
+            Console.WriteLine(intArray2[999]);
         }
     }
 }
