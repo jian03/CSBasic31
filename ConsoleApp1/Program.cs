c0b46116a6a280fa51025c42acb5a80dc6eadc2c
Jian Choi <s2019s19@e-mirim.hs.kr>
2021-05-03 15:29:17 +0900

    #18

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] intArray = { 1, 540, 33, 36, 20 };
+            int[] intArray = { 52, 172, 0, 13, 103 };
             Console.WriteLine(intArray[0]);
             Console.WriteLine(intArray[1]);
             Console.WriteLine(intArray[2]);
@@ -17,6 +17,33 @@ namespace ConsoleApp1
             int[] intArray2 = new int[1000];
             Console.WriteLine(intArray2[0]);
             Console.WriteLine(intArray2[999]);
+
+            int i = 0;
+            int[] intArray3 = { 52, 273, 32, 65, 103 };
+            while (i < intArray3.Length)
+            {
+                Console.WriteLine(i + "번째 출력: " + intArray3[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("입력(exit)를 입력하면 종료) : ");
+                input = Console.ReadLine();
+                switch(input)
+                {
+                    case "1":
+                        Console.WriteLine("go 공지사항");
+                        break;
+                    case "2":
+                        Console.WriteLine("go 오늘의 점심");
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다.");
+                        break;
+                }
+            } while (input != "exit");
         }
     }
 }
